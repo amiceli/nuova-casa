970bb78c77f37e62e6e43171fee70eb49dcad4ac
amiceli <antoine@miceli.click>
2026-07-24 18:00:53 +0200

    refactor(seeders): use updateOrInsert to make seeders idempotent 🌱

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -10,23 +10,31 @@ class PageSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        DB::table('pages')->insert(array(
-            'url' => 'https://github.com',
-            'title' => 'Github',
-            'created_at' => date('Y-m-d H:i:s'),
-            'favorite' => true,
-            'icon' => 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
-            'user_id' => 1,
-            'tag_id' => 1,
-        ));
-        DB::table('pages')->insert(array(
-            'url' => 'https://plantespourtous.co',
-            'created_at' => date('Y-m-d H:i:s'),
-            'title' => 'Plantes Pour Tous',
-            'favorite' => false,
-            'icon' => 'https://www.google.com/s2/favicons?domain=plantespourtous.co&sz=64',
-            'user_id' => 1,
-            'tag_id' => 1,
-        ));
+        DB::table('pages')->updateOrInsert(
+            array(
+                'tag_id' => 1,
+                'url' => 'https://github.com',
+            ),
+            array(
+                'title' => 'Github',
+                'created_at' => date('Y-m-d H:i:s'),
+                'favorite' => true,
+                'icon' => 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
+                'user_id' => 1,
+            )
+        );
+        DB::table('pages')->updateOrInsert(
+            array(
+                'tag_id' => 1,
+                'url' => 'https://plantespourtous.co',
+            ),
+            array(
+                'title' => 'Plantes Pour Tous',
+                'created_at' => date('Y-m-d H:i:s'),
+                'favorite' => false,
+                'icon' => 'https://www.google.com/s2/favicons?domain=plantespourtous.co&sz=64',
+                'user_id' => 1,
+            )
+        );
     }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -10,11 +10,15 @@ class TagSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        DB::table('tags')->insert(array(
-            'name' => 'Plante Pour Tous',
-            'created_at' => date('Y-m-d H:i:s'),
-            'icon' => 'https://www.google.com/s2/favicons?domain=plantespourtous.co&sz=64',
-            'user_id' => 1,
-        ));
+        DB::table('tags')->updateOrInsert(
+            array(
+                'name' => 'Plante Pour Tous',
+                'user_id' => 1,
+            ),
+            array(
+                'created_at' => date('Y-m-d H:i:s'),
+                'icon' => 'https://www.google.com/s2/favicons?domain=plantespourtous.co&sz=64',
+            )
+        );
     }
 }
