f960162cf2ccb409fd327d204850668895fe30cb
amiceli <antoine@miceli.click>
2026-07-25 16:02:31 +0200

    feat(seeders): seed github, plants, front-end and back-end links 🌱

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -10,15 +10,43 @@ class TagSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        DB::table('tags')->updateOrInsert(
+        $userId = DB::table('users')->min('id');
+
+        foreach ($this->tags() as $tag) {
+            DB::table('tags')->updateOrInsert(
+                array(
+                    'name' => $tag['name'],
+                    'user_id' => $userId,
+                ),
+                array(
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'icon' => $tag['icon'],
+                )
+            );
+        }
+    }
+
+    /**
+     * @return array<int, array<string, string>>
+     */
+    private function tags(): array {
+        return array(
             array(
-                'name' => 'Plante Pour Tous',
-                'user_id' => 1,
+                'name' => 'Github',
+                'icon' => 'https://www.google.com/s2/favicons?domain=github.com&sz=64',
             ),
             array(
-                'created_at' => date('Y-m-d H:i:s'),
+                'name' => 'Plante pour tous',
                 'icon' => 'https://www.google.com/s2/favicons?domain=plantespourtous.co&sz=64',
-            )
+            ),
+            array(
+                'name' => 'front-end',
+                'icon' => 'https://www.google.com/s2/favicons?domain=vuejs.org&sz=64',
+            ),
+            array(
+                'name' => 'back-end',
+                'icon' => 'https://www.google.com/s2/favicons?domain=laravel.com&sz=64',
+            ),
         );
     }
 }
