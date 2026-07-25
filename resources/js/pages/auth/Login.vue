aac5b10796d627a4657ad1d89d03378469f1bb4a
amiceli <antoine@miceli.click>
2026-07-25 21:48:41 +0200

    refactor(auth): wrap welcome and login pages in nord layout 🔐

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,51 +1,51 @@
 <template>
-    <nord-top-bar>
-        <strong class="n-typescale-l">{{ t("auth.brand") }}</strong>
-        <nord-button
-            slot="end"
-            variant="primary"
-            @click="goToAuth()"
-        >
-            {{ t("auth.login") }}
-        </nord-button>
-    </nord-top-bar>
-
-    <nord-stack
-        align-items="center"
-        class="n-padding-xl"
-    >
-        <nord-card padding="l">
-            <h1
-                slot="header"
-                class="n-typescale-xl"
-            >
-                {{ t("auth.welcomeTitle") }}
-            </h1>
-
-            <nord-stack>
-                <p class="n-color-text-weaker">{{ t("auth.welcomeSubtitle") }}</p>
-                <p class="n-color-text-weak">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p class="n-color-text-weak">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </nord-stack>
-
+    <nord-layout hide-default-nav-toggle>
+        <nord-top-bar slot="top-bar">
+            <strong class="n-typescale-l">{{ t("auth.brand") }}</strong>
             <nord-button
-                slot="footer"
+                slot="end"
                 variant="primary"
-                expand
                 @click="goToAuth()"
             >
-                {{ t("auth.loginWithGithub") }}
+                {{ t("auth.login") }}
             </nord-button>
-        </nord-card>
-    </nord-stack>
+        </nord-top-bar>
+
+        <nord-stack align-items="center">
+            <nord-card padding="l">
+                <h1
+                    slot="header"
+                    class="n-typescale-xl"
+                >
+                    {{ t("auth.welcomeTitle") }}
+                </h1>
+
+                <nord-stack>
+                    <p class="n-color-text-weaker">{{ t("auth.welcomeSubtitle") }}</p>
+                    <p class="n-color-text-weak">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+                    <p class="n-color-text-weak">
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </nord-stack>
+
+                <nord-button
+                    slot="footer"
+                    variant="primary"
+                    expand
+                    @click="goToAuth()"
+                >
+                    {{ t("auth.loginWithGithub") }}
+                </nord-button>
+            </nord-card>
+        </nord-stack>
+    </nord-layout>
 </template>
 
 <script setup lang="ts">
