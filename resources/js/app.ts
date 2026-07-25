28b3294da8dc75fe178180ec0716012d52d53d00
amiceli <antoine@miceli.click>
2026-07-25 16:01:08 +0200

    chore(deps): replace papierjs by nordhealth and drop vue-masonry 📦

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,3 +1,27 @@
+import "@nordhealth/themes/lib/nord.css"
+import "@nordhealth/css"
+import "@nordhealth/components/lib/Avatar"
+import "@nordhealth/components/lib/Badge"
+import "@nordhealth/components/lib/Banner"
+import "@nordhealth/components/lib/Button"
+import "@nordhealth/components/lib/Card"
+import "@nordhealth/components/lib/Dropdown"
+import "@nordhealth/components/lib/EmptyState"
+import "@nordhealth/components/lib/DropdownItem"
+import "@nordhealth/components/lib/Header"
+import "@nordhealth/components/lib/Icon"
+import "@nordhealth/components/lib/Input"
+import "@nordhealth/components/lib/Layout"
+import "@nordhealth/components/lib/Modal"
+import "@nordhealth/components/lib/Navigation"
+import "@nordhealth/components/lib/NavItem"
+import "@nordhealth/components/lib/Spinner"
+import "@nordhealth/components/lib/Stack"
+import "@nordhealth/components/lib/Toast"
+import "@nordhealth/components/lib/ToastGroup"
+import "@nordhealth/components/lib/Toggle"
+import "@nordhealth/components/lib/Tooltip"
+import "@nordhealth/components/lib/TopBar"
 import "../css/app.css"
 
 import { createInertiaApp } from "@inertiajs/vue3"
@@ -7,7 +31,6 @@ import { createApp, h } from "vue"
 import { ZiggyVue } from "ziggy-js"
 import { i18n } from "./i18n"
 import { initializeTheme } from "./composables/useAppearance"
-import MasonryWall from '@yeger/vue-masonry-wall'
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel"
 
@@ -19,7 +42,6 @@ createInertiaApp({
             .use(plugin)
             .use(i18n)
             .use(ZiggyVue)
-            .use(MasonryWall)
             .mount(el)
     },
     progress: {
