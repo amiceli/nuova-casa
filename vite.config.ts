dbd768002c9c3d436856b69fd38dd958ab428896
amiceli <antoine@miceli.click>
2026-07-26 15:02:22 +0200

    chore(config): add @assets path alias 🛠️

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./resources/js"),
+            "@assets": path.resolve(__dirname, "./resources/assets"),
         },
     },
 })
